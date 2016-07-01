8650bd5dbbeaadb6dc0ed67f97a85d0aedc0fdb3
Diana Teodorescu <dteodorescu@sorensonmedia.com>
2016-07-01 16:41:00 +0100

    - added server js to start page build

---
+++ src/Server.js
@@ -0,0 +1,32 @@
+// server.js
+
+// set up ========================
+var express  = require('express');
+var app      = express();                               // create our app w/ express
+var mongoose = require('mongoose');                     // mongoose for mongodb
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+// configuration =================
+
+mongoose.connect('localhost:27017/fridayprojectdata');     // connect to mongoDB database
+
+app.use(express.static(__dirname + '/../src/public'));
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+
+//add model to mongo
+//
+// var image = mongoose.model('image_info', {
+//     text: String
+// });
+
+
+// listen (start app with node server.js) ======================================
+app.listen(8080);
+console.log("App listening on port 8080");
